df3f7db2e67eeb5cd39c46a36b0dca446c8b9b78
gt-plus <gtorrealba.plus@gmail.com>
2025-02-02 10:09:56 -0400

    Primer respaldo

---
+++ js/Cl_grupo.js
@@ -0,0 +1,22 @@
+export default class Cl_grupo {
+  constructor() {
+    this.cnHombres = 0;
+    this.cnMujeres = 0;
+  }
+  procesarPersona(p) {
+    if (p.sexo === "M") {
+      this.cnHombres++;
+    } else {
+      this.cnMujeres++;
+    }
+  }
+  cantidadPersonas() {
+    return this.cnHombres + this.cnMujeres;
+  }
+  cantidadHombres() {
+    return this.cnHombres;
+  }
+  cantidadMujeres() {
+    return this.cnMujeres;
+  }
+}
